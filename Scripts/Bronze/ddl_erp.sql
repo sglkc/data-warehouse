225f39fd722ce83db88f331936494d7e3183ee7c
Seya <31957516+sglkc@users.noreply.github.com>
2025-07-11 14:57:23 +0700

    feat(Scripts): add bronze erp ddl sql

---
+++ Scripts/Bronze/ddl_erp.sql
@@ -0,0 +1,23 @@
+-- Create tables from ERP source
+-- Every column data type is a 1:1 replica from the source
+
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12 (
+  cid int
+  , bdate date
+  , gen varchar
+);
+
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101 (
+  cid int
+  , cntry varchar
+);
+
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+  id varchar
+  , cat varchar
+  , subcat varchar
+  , maintenance varchar
+);
