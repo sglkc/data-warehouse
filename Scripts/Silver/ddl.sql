287a37c70354d9e57fbd7278e8db02e2b5f0bdc7
Seya <31957516+sglkc@users.noreply.github.com>
2025-07-12 19:43:51 +0700

    fix(Scripts): change crm_sales_details int to date

--- Scripts/Silver/ddl.sql
+++ Scripts/Silver/ddl.sql
@@ -33,9 +33,9 @@ CREATE TABLE silver.crm_sales_details (
   sls_ord_num varchar
   , sls_prd_key varchar
   , sls_cust_id int
-  , sls_order_dt int
-  , sls_ship_dt int
-  , sls_due_dt int
+  , sls_order_dt date
+  , sls_ship_dt date
+  , sls_due_dt date
   , sls_sales int
   , sls_quantity int
   , sls_price int
