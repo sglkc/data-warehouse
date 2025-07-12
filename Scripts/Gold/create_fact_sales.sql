c853096eae822fa1e187438c4e35e4bee762b1dc
Seya <31957516+sglkc@users.noreply.github.com>
2025-07-12 20:46:43 +0700

    feat(Scripts): add gold fact_sales view

---
+++ Scripts/Gold/create_fact_sales.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE VIEW gold.fact_sales AS (
+  SELECT
+    csd.sls_ord_num order_number,
+    dp.product_key,
+    dc.customer_key,
+    csd.sls_order_dt order_date,
+    csd.sls_ship_dt ship_date,
+    csd.sls_due_dt due_date,
+    csd.sls_sales sales,
+    csd.sls_quantity quantity,
+    csd.sls_price price
+  FROM silver.crm_sales_details csd
+  JOIN gold.dim_customers dc
+    ON dc.customer_id = csd.sls_cust_id
+  JOIN gold.dim_products dp
+    ON dp.product_number = csd.sls_prd_key
+);
